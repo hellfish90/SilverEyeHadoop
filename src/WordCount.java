2df10d132a222176b7212a499dab9d048aabbd44
Marc Solé Farré <marcsolefarre90@gmail.com>
2016-11-02 20:28:17 +0100

    change db ports in input and output

--- src/WordCount.java
+++ src/WordCount.java
@@ -3,10 +3,6 @@
  */
 import java.io.*;
 
-import DAO.UserAnalyzedDAO;
-import Models.Collections;
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
@@ -15,6 +11,8 @@ import com.mongodb.hadoop.*;
 import com.mongodb.hadoop.util.*;
 import org.apache.htrace.commons.logging.Log;
 import org.apache.htrace.commons.logging.LogFactory;
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
 
 /**
  * test.in db.in.insert( { x : "eliot was here" } ) db.in.insert( { x : "eliot is here" } ) db.in.insert( { x : "who is
@@ -24,77 +22,26 @@ public class WordCount {
 
     private static final Log log = LogFactory.getLog(WordCount.class);
 
-    public static class IntSumReducer extends Reducer<LongWritable, Collections, LongWritable, Collections> {
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Object, BSONObject> {
 
-        private final IntWritable result = new IntWritable();
-        private UserAnalyzedDAO userAnalyzedDAO;
-
-        protected void setup(Context context) throws IOException, InterruptedException {
-            MongoClient mongo = new MongoClient("localhost", 27017);
-            MongoDatabase db = mongo.getDatabase("SilverEye");
-            userAnalyzedDAO = new UserAnalyzedDAO(db);
-
-
-        }
-
-        public void reduce( LongWritable key, Iterable<Collections> values, Context context )
+        public void reduce( Text key, Iterable<IntWritable> values, Context context )
                 throws IOException, InterruptedException{
 
+                Integer totalValue = 0;
 
-
-            Collections collections = new Collections();
-
-            for (Collections collection: values){
-
-                //Plus positive collections
-                for (Writable positiveCollection: collection.getPositiveCollections().keySet()){
-
-                    String collectionKey = positiveCollection.toString();
-                    Long value = ((LongWritable)collection.getPositiveCollections().get(positiveCollection)).get();
-                    collections.plusPositiveCollection(collectionKey,value);
-
+                for (IntWritable value: values){
+                    totalValue += value.get();
                 }
 
-                //Plus negative collections
-                for (Writable negativeCollection: collection.getNegativeCollections().keySet()){
-
-                    String collectionKey = negativeCollection.toString();
-                    Long value = ((LongWritable)collection.getNegativeCollections().get(negativeCollection)).get();
-                    collections.plusNegativeCollection(collectionKey,value);
-
-                }
-
-                //Plus neutral collections
-                for (Writable neutralCollection: collection.getNeutralCollections().keySet()){
-
-                    String collectionKey = neutralCollection.toString();
-                    Long value = ((LongWritable)collection.getNeutralCollections().get(neutralCollection)).get();
-                    collections.plusNeutralCollection(collectionKey,value);
-
-                }
-
-                //Plus counter collections
-                for (Writable counterCollection: collection.getCounterCollection().keySet()){
-
-                    String collectionKey = counterCollection.toString();
-                    Long value = ((LongWritable)collection.getCounterCollection().get(counterCollection)).get();
-                    collections.plusCounterCollection(collectionKey,value);
-
-                }
-
-            }
-
-            userAnalyzedDAO.saveUser(key.get(),collections.getPositiveCollections(),
-                    collections.getNegativeCollections(), collections.getNeutralCollections(),
-                    collections.getCounterCollection());
+                context.write(key, new BasicBSONObject("value",totalValue));
         }
     }
 
     public static void main( String[] args ) throws Exception{
 
         final Configuration conf = new Configuration();
-        MongoConfigUtil.setInputURI( conf, "mongodb://localhost/SilverEye.AnalyzedTweet" );
-        MongoConfigUtil.setOutputURI( conf, "mongodb://localhost/SilverEye.AnalyzedUser" );
+        MongoConfigUtil.setInputURI( conf, "mongodb://localhost:27017/Sentiment.NegativeTweet" );
+        MongoConfigUtil.setOutputURI( conf, "mongodb://localhost:2126/Sentiment.NegativeWords" );
 
         System.out.println( "Conf: " + conf );
 
@@ -102,13 +49,13 @@ public class WordCount {
 
         job.setJarByClass( WordCount.class );
 
-        job.setMapperClass( UsersMapper.class );
+        job.setMapperClass( WordsMapper.class );
 
         //job.setCombinerClass( IntSumReducer.class );
         job.setReducerClass( IntSumReducer.class );
 
-        job.setOutputKeyClass( LongWritable.class );
-        job.setOutputValueClass( Collections.class );
+        job.setOutputKeyClass( Text.class );
+        job.setOutputValueClass( IntWritable.class );
 
         job.setInputFormatClass( MongoInputFormat.class );
         job.setOutputFormatClass( MongoOutputFormat.class );
